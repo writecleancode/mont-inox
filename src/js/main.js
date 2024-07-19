5a70965aea6cf1c1a7a02cd25010734a7850f646
Rafal <rafek131@wp.pl>
2024-07-19 14:49:44 +0200

    completed navigation for devices of screen width =320px

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,9 @@
 const burgerBtnElement = document.querySelector('.navbar__burger-btn');
+const navbarLinksListElement = document.querySelector('.navbar__links');
 
-const handleMovileNavigation = () => burgerBtnElement.classList.toggle('active');
+const handleMovileNavigation = () => {
+	burgerBtnElement.classList.toggle('active');
+	navbarLinksListElement.classList.toggle('active');
+};
 
 burgerBtnElement.addEventListener('click', handleMovileNavigation);
