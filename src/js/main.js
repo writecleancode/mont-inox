a51e004cb6cddef4a262a91f901c3139c6c3e243
Rafal <rafek131@wp.pl>
2024-07-22 18:23:19 +0200

    mobile navigation is now closed after click on a link or after click outside of the navigation

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,24 @@
+const navbarElement = document.querySelector('.navbar');
 const burgerBtnElement = document.querySelector('.navbar__burger-btn');
 const navbarLinksListElement = document.querySelector('.navbar__links');
+const navbarLinks = document.querySelectorAll('[data-nav]');
 
 const handleMovileNavigation = () => {
 	burgerBtnElement.classList.toggle('active');
 	navbarLinksListElement.classList.toggle('active');
 };
 
+const handleCloseMobileNav = () => {
+	if (!navbarLinksListElement.classList.contains('active')) return;
+
+	burgerBtnElement.classList.remove('active');
+	navbarLinksListElement.classList.remove('active');
+};
+
+const checkClick = e => {
+	!navbarElement.contains(e.target) && handleCloseMobileNav();
+};
+
 burgerBtnElement.addEventListener('click', handleMovileNavigation);
+navbarLinks.forEach(item => item.addEventListener('click', handleCloseMobileNav));
+document.addEventListener('click', checkClick);
